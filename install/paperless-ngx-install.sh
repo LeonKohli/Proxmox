b3f8b07b5fb2b0a64a15bb86ff5f78498e959426
LeonKohli <98176333+LeonKohli@users.noreply.github.com>
2024-05-30 23:43:40 +0200

    Update paperless-ngx-install.sh

--- install/paperless-ngx-install.sh
+++ install/paperless-ngx-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2024 tteck
-# Author: tteck (tteckster)
+# Author: tteck (tteckster)  
 # License: MIT
 # https://github.com/tteck/Proxmox/raw/main/LICENSE
 
@@ -26,7 +26,7 @@ $STD apt-get install -y --no-install-recommends \
   libmagic-dev \
   mime-support \
   libzbar0 \
-  poppler-utils \
+  poppler-utils \  
   default-libmysqlclient-dev \
   automake \
   libtool \
@@ -40,7 +40,7 @@ $STD apt-get install -y --no-install-recommends \
   mc
 msg_ok "Installed Dependencies"
 
-msg_info "Installing Python3 Dependencies"
+msg_info "Installing Python3 Dependencies" 
 $STD apt-get install -y --no-install-recommends \
   python3 \
   python3-pip \
@@ -60,12 +60,12 @@ $STD apt-get install -y --no-install-recommends \
   pngquant \
   zlib1g \
   tesseract-ocr \
-  tesseract-ocr-eng
+  tesseract-ocr-eng  
 msg_ok "Installed OCR Dependencies"
 
 msg_info "Installing JBIG2"
 $STD git clone https://github.com/agl/jbig2enc /opt/jbig2enc
-cd /opt/jbig2enc
+cd /opt/jbig2enc  
 $STD bash ./autogen.sh
 $STD bash ./configure
 $STD make
@@ -86,39 +86,46 @@ $STD pip install -r requirements.txt
 curl -s -o /opt/paperless/paperless.conf https://raw.githubusercontent.com/paperless-ngx/paperless-ngx/main/paperless.conf.example
 mkdir -p {consume,data,media,static}
 sed -i -e 's|#PAPERLESS_REDIS=redis://localhost:6379|PAPERLESS_REDIS=redis://localhost:6379|' /opt/paperless/paperless.conf
-sed -i -e "s|#PAPERLESS_CONSUMPTION_DIR=../consume|PAPERLESS_CONSUMPTION_DIR=/opt/paperless/consume|" /opt/paperless/paperless.conf
+sed -i -e "s|#PAPERLESS_CONSUMPTION_DIR=../consume|PAPERLESS_CONSUMPTION_DIR=/opt/paperless/consume|" /opt/paperless/paperless.conf  
 sed -i -e "s|#PAPERLESS_DATA_DIR=../data|PAPERLESS_DATA_DIR=/opt/paperless/data|" /opt/paperless/paperless.conf
 sed -i -e "s|#PAPERLESS_MEDIA_ROOT=../media|PAPERLESS_MEDIA_ROOT=/opt/paperless/media|" /opt/paperless/paperless.conf
 sed -i -e "s|#PAPERLESS_STATICDIR=../static|PAPERLESS_STATICDIR=/opt/paperless/static|" /opt/paperless/paperless.conf
-echo "${Paperlessngx}" >/opt/${APPLICATION}_version.txt
+echo "${Paperlessngx}" >/opt/${APPLICATION}_version.txt  
 msg_ok "Installed Paperless-ngx"
 
 msg_info "Installing Natural Language Toolkit (Patience)"
 $STD python3 -m nltk.downloader -d /usr/share/nltk_data all
 msg_ok "Installed Natural Language Toolkit"
 
+msg_info "Installing Apache Tika for Office Document Support"
+$STD apt-get install -y openjdk-11-jre-headless
+$STD wget https://archive.apache.org/dist/tika/tika-server-2.6.0.jar -O /opt/paperless/tika-server.jar
+sed -i -e 's|#PAPERLESS_TIKA_ENABLED=false|PAPERLESS_TIKA_ENABLED=true|' /opt/paperless/paperless.conf
+sed -i -e 's|#PAPERLESS_TIKA_ENDPOINT=http://localhost:9998|PAPERLESS_TIKA_ENDPOINT=http://localhost:9998|' /opt/paperless/paperless.conf
+msg_ok "Installed Apache Tika"
+
 msg_info "Setting up PostgreSQL database"
 DB_NAME=paperlessdb
-DB_USER=paperless
+DB_USER=paperless  
 DB_PASS="$(openssl rand -base64 18 | cut -c1-13)"
 SECRET_KEY="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)"
 $STD sudo -u postgres psql -c "CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASS';"
 $STD sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER ENCODING 'UTF8' TEMPLATE template0;"
 $STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET client_encoding TO 'utf8';"
-$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET default_transaction_isolation TO 'read committed';"
+$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET default_transaction_isolation TO 'read committed';"  
 $STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET timezone TO 'UTC'"
 echo "" >>~/paperless.creds
 echo -e "Paperless-ngx Database User: \e[32m$DB_USER\e[0m" >>~/paperless.creds
-echo -e "Paperless-ngx Database Password: \e[32m$DB_PASS\e[0m" >>~/paperless.creds
+echo -e "Paperless-ngx Database Password: \e[32m$DB_PASS\e[0m" >>~/paperless.creds  
 echo -e "Paperless-ngx Database Name: \e[32m$DB_NAME\e[0m" >>~/paperless.creds
 sed -i -e 's|#PAPERLESS_DBHOST=localhost|PAPERLESS_DBHOST=localhost|' /opt/paperless/paperless.conf
 sed -i -e 's|#PAPERLESS_DBPORT=5432|PAPERLESS_DBPORT=5432|' /opt/paperless/paperless.conf
 sed -i -e "s|#PAPERLESS_DBNAME=paperless|PAPERLESS_DBNAME=$DB_NAME|" /opt/paperless/paperless.conf
-sed -i -e "s|#PAPERLESS_DBUSER=paperless|PAPERLESS_DBUSER=$DB_USER|" /opt/paperless/paperless.conf
+sed -i -e "s|#PAPERLESS_DBUSER=paperless|PAPERLESS_DBUSER=$DB_USER|" /opt/paperless/paperless.conf  
 sed -i -e "s|#PAPERLESS_DBPASS=paperless|PAPERLESS_DBPASS=$DB_PASS|" /opt/paperless/paperless.conf
 sed -i -e "s|#PAPERLESS_SECRET_KEY=change-me|PAPERLESS_SECRET_KEY=$SECRET_KEY|" /opt/paperless/paperless.conf
 cd /opt/paperless/src
-$STD python3 manage.py migrate
+$STD python3 manage.py migrate  
 msg_ok "Set up PostgreSQL database"
 
 read -r -p "Would you like to add Adminer? <y/N> " prompt
@@ -135,7 +142,7 @@ if [[ "${prompt,,}" =~ ^(y|yes)$ ]]; then
   echo -e "Adminer Username: \e[32m$DB_USER\e[0m" >>~/paperless.creds
   echo -e "Adminer Password: \e[32m$DB_PASS\e[0m" >>~/paperless.creds
   echo -e "Adminer Database: \e[32m$DB_NAME\e[0m" >>~/paperless.creds
-  msg_ok "Installed Adminer"
+  msg_ok "Installed Adminer"  
 fi
 
 msg_info "Setting up admin Paperless-ngx User & Password"
@@ -164,11 +171,11 @@ Requires=redis.service
 WorkingDirectory=/opt/paperless/src
 ExecStart=celery --app paperless beat --loglevel INFO
 
-[Install]
+[Install]  
 WantedBy=multi-user.target
 EOF
 
-cat <<EOF >/etc/systemd/system/paperless-task-queue.service
+cat <<EOF >/etc/systemd/system/paperless-task-queue.service  
 [Unit]
 Description=Paperless Celery Workers
 Requires=redis.service
@@ -178,11 +185,11 @@ WorkingDirectory=/opt/paperless/src
 ExecStart=celery --app paperless worker --loglevel INFO
 
 [Install]
-WantedBy=multi-user.target
+WantedBy=multi-user.target  
 EOF
 
 cat <<EOF >/etc/systemd/system/paperless-consumer.service
-[Unit]
+[Unit]  
 Description=Paperless consumer
 Requires=redis.service
 
@@ -196,12 +203,12 @@ EOF
 
 cat <<EOF >/etc/systemd/system/paperless-webserver.service
 [Unit]
-Description=Paperless webserver
+Description=Paperless webserver  
 After=network.target
 Wants=network.target
 Requires=redis.service
 
-[Service]
+[Service]  
 WorkingDirectory=/opt/paperless/src
 ExecStart=/usr/local/bin/gunicorn -c /opt/paperless/gunicorn.conf.py paperless.asgi:application
 
@@ -209,10 +216,24 @@ ExecStart=/usr/local/bin/gunicorn -c /opt/paperless/gunicorn.conf.py paperless.a
 WantedBy=multi-user.target
 EOF
 
+cat <<EOF >/etc/systemd/system/paperless-tika.service
+[Unit]
+Description=Apache Tika Server for Paperless
+After=network.target
+Wants=network.target
+
+[Service]
+WorkingDirectory=/opt/paperless
+ExecStart=/usr/bin/java -jar /opt/paperless/tika-server.jar
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
 sed -i -e 's/rights="none" pattern="PDF"/rights="read|write" pattern="PDF"/' /etc/ImageMagick-6/policy.xml
 
-systemctl daemon-reload
-$STD systemctl enable --now paperless-consumer paperless-webserver paperless-scheduler paperless-task-queue.service
+systemctl daemon-reload  
+$STD systemctl enable --now paperless-consumer paperless-webserver paperless-scheduler paperless-task-queue paperless-tika
 msg_ok "Created Services"
 
 motd_ssh
